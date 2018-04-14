3913e208ba7a4a1f8221ea3a883e13da64289d23
Eduardo Henrique <dev_eduardo@hotmail.com>
2018-04-14 19:10:43 -0300

    animando placar

--- public/js/placar.js
+++ public/js/placar.js
@@ -1,21 +1,26 @@
-function inserePlacar(){
+$("#btn-placar").click(mostraPlacar);
+
+function inserePlacar() {
     let placar = $('.placar');
     let corpoPlacar = placar.find("tbody");
     let usuario = "Eduardo";
     let nPalavras = $('#contador-palavras').text();
 
-    let linha = novaLinha(usuario,nPalavras);
+    let linha = novaLinha(usuario, nPalavras);
 
     corpoPlacar.prepend(linha);
+
+    $('.placar').slideDown(500);
+    scrollPlacar();
 }
 
-function novaLinha(usuario,palavaras){
+function novaLinha(usuario, palavaras) {
     let linha = $("<tr>");
     let colunaUsuario = $("<td>").text(usuario);
     let colunaPalavras = $("<td>").text(palavaras);
     let colunaRemover = $("<td>");
 
-    let link = $("<a>").attr("href","#").addClass("botao-remover");
+    let link = $("<a>").attr("href", "#").addClass("botao-remover");
     let icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
 
     link.append(icone);
@@ -28,7 +33,24 @@ function novaLinha(usuario,palavaras){
     return linha;
 }
 
-function removeLinha(event){
+function removeLinha(event) {
     event.preventDefault();
-    $(this).parent().parent().remove();
+    let linha = $(this).parent().parent();
+
+    linha.fadeOut();
+    setTimeout(function () {
+        linha.remove();
+    });
+}
+
+function mostraPlacar() {
+    $(".placar").stop().slideToggle(600);
+}
+
+function scrollPlacar() {
+    let posicaoPlacar = $('.placar').offset().top;
+    $("html").animate(
+        {
+            scrollTop: posicaoPlacar + "px"
+        },1000);
 }
