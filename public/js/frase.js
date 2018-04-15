c4f7bb5537d5ee72edb382bfe05d230921c0f0bf
Eduardo Henrique <dev_eduardo@hotmail.com>
2018-04-15 10:08:11 -0300

    adicionando erro e spinner e melhorando a usabilidade

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,10 +1,21 @@
 $('#btn-frase').click(fraseAleatoria);
 
-function fraseAleatoria(){
-    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+function fraseAleatoria() {
+    $("#spinner").toggle();
+
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria)
+        .fail(function () {
+            $("#erro").toggle();
+            setTimeout(function () {
+                $("#erro").toggle();
+            }, 2000);
+        })
+        .always(function(){
+            $("#spinner").toggle();
+        });
 }
 
-function trocaFraseAleatoria(data){
+function trocaFraseAleatoria(data) {
     let frase = $('.frase');
     let numeroAleatorio = Math.floor(Math.random() * data.length);
     console.log(data);
